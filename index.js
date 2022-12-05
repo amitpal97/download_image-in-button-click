755c460a01cb17ed6681c794719c26886226551e
amitpal97 <amitrpal9@gmail.com>
2022-12-05 11:51:06 +0530

    everying done and also done with dom mainuplation insert images

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
-var image= document.querySelector(".img");
-var btndiv=document.querySelector(".btnnew");
-var button= document.querySelector(".btn").addEventListener("click",function(){
-    // image.classList.add("color")
-    // alert("Your image downloaded.")
-    // e.preventDefault();
-    fatchData(image);
-});
+var image = document.querySelectorAll(".img");
+// .forEach((e)=>{
+// console.log(e);
+// })
 
-function fatchData(data){
-    fetch(data).then(res => res.blob()) .then(file => {
-       let tempUrl= URL.createObjectURL(file);
-       console.log(tempUrl );
-       let aTagInject= document.createElement("a");
-        aTagInject.href=tempUrl;
-        aTagInject.download="filename";
-        document.body.appendChild(aTagInject);
-        aTagInject.click();
-        aTagInject.remove();
-    });
-}
+// var btndiv = document.querySelectorAll(".btnnew");
+
+// var mainWrapper = document.querySelectorAll(".wrapper");
+// .forEach((e)=>{
+// e.classList.toggle("color")
+// })
+
+// console.log(mainWrapper);
+
+var button = document
+  .querySelector(".btn")
+  .addEventListener("click", function (e) {
+    // image.classList.add("color")
+    // alert(" your image now  downloading.")
+    var data = ["/assets/Group 579.png" , "/assets/Group 579.png" , "/assets/Group 579.png"];
+    for (i=0 ; i<data.length ; i++) {
+        var a = document.createElement("a");
+        a.href = data[i];
+       
+        a.download = data[i];
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+      }
+
+    e.preventDefault();
+    fatchData(image);
+  });
+// button.foreach((elm)=>{
+// console.log(elm);
+// })
+
+// function fatchData(data) {
+//   fetch(data.src)
+//     .then((res) => res.blob())
+//     .then((file) => {
+//       let tempUrl = URL.createObjectURL(file);
+//       //    console.log(tempUrl );
+//       let aTagInject = document.createElement("a");
+//       aTagInject.href = tempUrl;
+//       aTagInject.download = data.src;
+//       document.body.appendChild(aTagInject);
+//       aTagInject.click();
+//       aTagInject.remove();
+//     });
+// }
+
 
